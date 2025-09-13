b7468e5f1ded663a0cdf91ebcf4b2190d60e3db8
Sang-Yun Oh (Sang) <syoh@ucsb.edu>
2025-09-13 21:22:44 +0000

    fix: update devcontainer configurations and Dockerfiles for consistency; add PyTorch matrix multiplication test script

--- slurm-gpu.sh
+++ slurm-gpu.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=GPU_Job
-#SBATCH --time=01:30:00
-#SBATCH --ntasks=1
-#SBATCH --mem=16G
-#SBATCH --output=testing/gpu_test_output.%j
-#SBATCH --partition=cpu
+
+#SBATCH -J pytorch_matmul_scaling_test       # Job name
+#SBATCH -o pytorch_matmul_scaling_test.%j    # stdout output file (%j is jobId)
+#SBATCH -p h100                              # Queue name
+#SBATCH -N 1                                 # Number of nodes requested
+#SBATCH -n 1                                 # Number of mpi tasks requested
+#SBATCH -t 00:10:00                          # Run time (hh:mm:ss)
+
 cd $SCRATCH
-export SINGULARITY_CACHEDIR=$TMPDIR/.singularity
-module load WebProxy
-singularity pull customized_docker.sif docker://ghcr.io/392781/4-customized:latest
-singularity exec customized_docker.sif radian --version
-exit
+
+module load tacc-apptainer
+
+echo "### Using CPUs"
+apptainer exec --nv pytorch.sif python pytorch_matmul_scaling_test.py --no-gpu
+
+echo "### Using GPUs"
+apptainer exec --nv pytorch.sif python pytorch_matmul_scaling_test.py
